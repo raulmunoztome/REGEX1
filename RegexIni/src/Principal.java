bc28bac137a2ff51fa0adc31112cce25cbbb46bc
cfgs <cfgs@pc-info-host>
2026-02-10 18:00:56 +0100

    Cambiando

--- RegexIni/src/Principal.java
+++ RegexIni/src/Principal.java
@@ -2,13 +2,26 @@
 public class Principal {
 
 	public static void main(String[] args) {
-		
-		System.out.println(RegexClase.ValidarDNI("47667949N"));
-		System.out.println(RegexClase.ValidarEMAIL("raul@hotmail.com"));
-		System.out.println(RegexClase.ValidarEMAIL("ral@hotl.com"));
-		System.out.println(RegexClase.ValidarEMAIL("raul@hotmailes"));
-		System.out.println(RegexClase.ValidarEMAIL("john.doe123@mail.net"));
-		System.out.println(RegexClase.ValidarEMAIL("1john.doe123@mail.net"));
+		System.out.println("DNI: ");
+		System.out.println("47667949N "+RegexClase.ValidarDNI("47667949N"));
+		System.out.println("EMAILS:");
+		System.out.println("raul@hotmail.com "+RegexClase.ValidarEMAIL("raul@hotmail.com"));
+		System.out.println("ral@hotl.com "+RegexClase.ValidarEMAIL("ral@hotl.com"));
+		System.out.println("rl@hotl.com "+RegexClase.ValidarEMAIL("rl@hotl.com"));
+		System.out.println("raul@hotmailes "+RegexClase.ValidarEMAIL("raul@hotmailes"));
+		System.out.println("john.doe123@mail.net "+RegexClase.ValidarEMAIL("john.doe123@mail.net"));
+		System.out.println("1john.doe123@mail.net "+RegexClase.ValidarEMAIL("1john.doe123@mail.net"));
+		System.out.println("TEXTO Y NUMEROS:");
+		System.out.println("Este texto tiene 43 numer0s y letras 111 "+RegexClase.numeroLetras("Este texto tiene 43 numer0s y letras 111"));
+		System.out.println("Este texto tiene cositas raras '- y a90 "+RegexClase.numeroLetras("Este texto tiene cositas raras '- y a90"));
+		System.out.println("NUMERO DE TELEFONO: ");
+		System.out.println("'+34 612345678' "+RegexClase.validarTLF("+34 612345678"));
+		System.out.println("'612 34 56 78' "+RegexClase.validarTLF("612 34 56 78"));
+		System.out.println("'612345678' "+RegexClase.validarTLF("612345678"));
+		System.out.println("'+9555 4' "+RegexClase.validarTLF("+9555 4"));
+		System.out.println("CONTRASEÑA SEGURA:");
+		System.out.println("B-3456a-AN "+RegexClase.passCorrect("B-3456a-AN"));
+		System.out.println("B-3456-AN "+RegexClase.passCorrect("B-3456-AN"));
 	}
 
 }
--- RegexIni/src/RegexClase.java
+++ RegexIni/src/RegexClase.java
@@ -6,7 +6,16 @@ public class RegexClase {
 		return Pattern.matches("[0-9]{8}[A-Za-z]",texto);
 	}
 	public static boolean ValidarEMAIL(String em) {
-		return Pattern.matches("^[A-Za-z0-9\\.]*@[A-Za-z]{3,}\\.[A-Za-z]{2,}", em);
+		return Pattern.matches("^[A-Za-z0-9\\.]{3,}@[A-Za-z]{3,}\\.[A-Za-z]{2,}", em);
+	}
+	public static boolean numeroLetras(String intro) {
+		return Pattern.matches("[A-Za-z0-9\s*]{1,}", intro);
+	}
+	public static boolean validarTLF(String num) {
+		return Pattern.matches("\\+?[0-9\s*]{8,}", num);
+	}
+	public static boolean passCorrect(String contra) {
+		return Pattern.matches("^(?=.[A-Z])+(?=.[a-z])+(?=.[0-9])+(?=.[@#$%&-])+{8,}", contra);
 	}
 	
 }
