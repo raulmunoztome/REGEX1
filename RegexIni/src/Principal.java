3be84e36298c8526a94701a86811ed9b71b044e7
raulmunoztome <raulmunoztome@gmail.com>
2026-02-11 03:58:00 +0100

    Finalizado

--- RegexIni/src/Principal.java
+++ RegexIni/src/Principal.java
@@ -3,29 +3,37 @@ public class Principal {
 
 	public static void main(String[] args) {
 		System.out.println("DNI: ");
-		System.out.println("47667949N "+RegexClase.ValidarDNI("47667949N"));
+		System.out.println("47667949N "+RegexClase.ValidarDNI("47667949N")+"\n");
 		
 		System.out.println("EMAILS:");
 		System.out.println("raul@hotmail.com "+RegexClase.ValidarEMAIL("raul@hotmail.com"));
-		System.out.println("ral@hotl.com "+RegexClase.ValidarEMAIL("ral@hotl.com"));
-		System.out.println("rl@hotl.com "+RegexClase.ValidarEMAIL("rl@hotl.com"));
+		System.out.println("ral@hotl.es "+RegexClase.ValidarEMAIL("ral@hotl.es"));
+		System.out.println("rap@ht.com "+RegexClase.ValidarEMAIL("rap@ht.com"));
 		System.out.println("raul@hotmailes "+RegexClase.ValidarEMAIL("raul@hotmailes"));
 		System.out.println("john.doe123@mail.net "+RegexClase.ValidarEMAIL("john.doe123@mail.net"));
-		System.out.println("1john.doe123@mail.net "+RegexClase.ValidarEMAIL("1john.doe123@mail.net"));
+		System.out.println("1john.doe123@mail.net "+RegexClase.ValidarEMAIL("1john.doe123@mail.net")+"\n");
 		
 		System.out.println("TEXTO Y NUMEROS:");
 		System.out.println("'Este texto tiene 43 numer0s y letras 111' "+RegexClase.numeroLetras("Este texto tiene 43 numer0s y letras 111"));
-		System.out.println("'Este texto tiene cositas-p raras - y a90' "+RegexClase.numeroLetras("Este texto tiene cositas raras '- y a90"));
+		System.out.println("'Este texto tiene cositas-p raras - y a90' "+RegexClase.numeroLetras("Este texto tiene cositas raras '- y a90")+"\n");
 		
 		System.out.println("NUMERO DE TELEFONO: ");
 		System.out.println("'+34 612345678' "+RegexClase.validarTLF("+34 612345678"));
 		System.out.println("'612 34 56 78' "+RegexClase.validarTLF("612 34 56 78"));
 		System.out.println("'612345678' "+RegexClase.validarTLF("612345678"));
-		System.out.println("'+9555 4' "+RegexClase.validarTLF("+9555 4"));
+		System.out.println("'+9555 4' "+RegexClase.validarTLF("+9555 4")+"\n");
 		
 		System.out.println("CONTRASEÑA SEGURA:");
 		System.out.println("B-3456a-AN "+RegexClase.passCorrect("B-3456a-AN"));
-		System.out.println("B-3456-AN "+RegexClase.passCorrect("B-3456-AN"));
+		System.out.println("B-3456-AN "+RegexClase.passCorrect("B3456-AN")+"\n");
+		
+		System.out.println("MATRICULAS: ");
+		System.out.println("B-3456-AN "+RegexClase.matriculaInfo("B-3456-AN"));
+		System.out.println("HU-1234-X "+RegexClase.matriculaInfo("HU-1234-X"));
+		System.out.println("7890-MXZ "+RegexClase.matriculaInfo("7890-MXZ"));
+		System.out.println("5645-AXZ "+RegexClase.matriculaInfo("5645-AXZ"));
+		System.out.println("R-5645-AN "+RegexClase.matriculaInfo("R-5645-AN"));
+		
 	}
 
 }
--- RegexIni/src/RegexClase.java
+++ RegexIni/src/RegexClase.java
@@ -1,12 +1,15 @@
+import java.util.Arrays;
+import java.util.List;
 import java.util.regex.Pattern;
 
 public class RegexClase {
+	private static final List<String> CODIGOS_PROVINCIALES = Arrays.asList("A", "AB", "AL", "AV", "B", "BA", "BI", "BU", "C", "CA", "CC", "CS", "CR", "CO", "CU", "GE", "GR", "GU", "H", "HU", "J", "L", "LE", "LO", "LU", "M", "MA", "MU", "O", "OR", "P", "NA", "PM", "PO", "S", "SA", "SS", "SG", "SE", "SO", "T", "TE", "GC", "TF", "TO", "V", "VA", "VI", "Z", "ZA", "CE", "ML", "GI", "OU", "IB");
 	
 	public static boolean ValidarDNI(String texto) {
 		return Pattern.matches("[0-9]{8}[A-Za-z]",texto);
 	}
-	public static boolean ValidarEMAIL(String em) {
-		return Pattern.matches("^[A-Za-z0-9\\.]{3,}@[A-Za-z]{3,}\\.[A-Za-z]{2,}", em);
+	public static boolean ValidarEMAIL(String email) {
+		return Pattern.matches("^[A-Za-z0-9\\.]{3,}@[A-Za-z]{3,}\\.[A-Za-z]{2,}", email);
 	}
 	public static boolean numeroLetras(String intro) {
 		return Pattern.matches("[A-Za-z0-9\s]{1,}", intro);
@@ -17,5 +20,25 @@ public class RegexClase {
 	public static boolean passCorrect(String contra) {
 		return Pattern.matches("^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[@#$%&-]).{8,}$", contra);
 	}
-	
+	public static boolean matriculaInfo(String matricula) {
+		
+		if(Pattern.matches("^[0-9]{4}-[BCDFGHJKLMNPRSTVWXYZ]{3}$", matricula)) {
+			
+			return true;
+		}
+		
+		int index = 0;
+		String codigo;
+		
+		while(index < CODIGOS_PROVINCIALES.size()){
+			
+			codigo = CODIGOS_PROVINCIALES.get(index);
+			if(Pattern.matches("^"+codigo+"-[0-9]{4}-[A-Z]{1,2}$", matricula)) {
+				
+				return true;
+			}
+			else ++index;
+		}
+		return false;
+	}
 }
